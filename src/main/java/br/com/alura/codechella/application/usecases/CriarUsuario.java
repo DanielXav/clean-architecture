26ed3f40b89aa4e909862d7dd2cc385d0ddb5887
Daniel Xavier <dxavier194@gmail.com>
2024-06-20 20:06:59 -0300

    adicionado o controlador

--- src/main/java/br/com/alura/codechella/application/usecases/CriarUsuario.java
+++ src/main/java/br/com/alura/codechella/application/usecases/CriarUsuario.java
@@ -4,7 +4,9 @@ package br.com.alura.codechella.application.usecases;
 import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CriarUsuario {
 
     private final RepositorioDeUsuario repositorio;
